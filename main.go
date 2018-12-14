4bcc13ed0023380c318b679a3df02c44c6a050e2
Thomas Kobber Panum <thomas@panum.dk>
2018-12-14 14:47:33 +0100

    Fixed race condition

--- main.go
+++ main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 type server struct {
+	m sync.Mutex
 	i int
 }
 
 func (s *server) handleRead(w http.ResponseWriter, r *http.Request) {
+	s.m.Lock()
 	fmt.Fprintf(w, "%d", s.i)
+	s.m.Unlock()
 }
 
 func (s *server) handleInc(w http.ResponseWriter, r *http.Request) {
+	s.m.Lock()
 	s.i = s.i + 1
+	s.m.Unlock()
 	fmt.Fprintf(w, "ok")
 }
 
